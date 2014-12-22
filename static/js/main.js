1a31830ebb48301e6df576189d027aed01084184
JerryPark <eaeao@naver.com>
2014-12-22 13:12:06 +0900

    버그수정

--- static/js/main.js
+++ static/js/main.js
@@ -29,7 +29,7 @@ $(window).load(function() {
 
 function loadGameLinkAccount(account_id){
     $('#GamelinkModal .modal-content').load("/head/gamelink/",{'csrfmiddlewaretoken':$('input[name=csrfmiddlewaretoken]').val()},function(){
-        $("#GamelinkModal #div_tutor_right").html('<img src="/static/img/head/ajax-loader_s.gif" style="margin-top:160px">');
+        $("#GamelinkModal #div_tutor_right").html('<img src="/static/img/ajax-loader_s.gif" style="margin-top:160px">');
         $("#GamelinkModal #div_tutor_right").hide().load("/head/gamelink/load/",{
             "csrfmiddlewaretoken":$("#header input[name=csrfmiddlewaretoken]").val(),
             "account_id":account_id
